10c98e6ba49ce457be8cdaf93540cbe1f7905412
CMHopeSunshine <277073121@qq.com>
2022-12-07 14:16:45 +0800

    ✨ 修复`ysc`报错

--- LittlePaimon/utils/requests.py
+++ LittlePaimon/utils/requests.py
@@ -96,6 +96,7 @@ class aiorequests:
                                             headers=headers,
                                             params=params,
                                             timeout=timeout,
+                                            follow_redirects=True,
                                             **kwargs)
                     # 不保存安柏计划的问号图标
                     if resp.headers.get('etag') == 'W/"6363798a-13c7"' or resp.headers.get(
@@ -111,6 +112,7 @@ class aiorequests:
                                             headers=headers,
                                             params=params,
                                             timeout=timeout,
+                                            follow_redirects=True,
                                             **kwargs)
                     if resp.headers.get('etag') == 'W/"6363798a-13c7"' or resp.headers.get(
                             'content-md5') == 'JeG5b/z8SpViMmO/E9eayA==':
