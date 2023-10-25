5ae8e6f6e3e319f2d76301c71b8d88e365c84ec7
forchannot <100580891+forchannot@users.noreply.github.com>
2023-10-26 06:52:11 +0800

    去除原神武器图鉴链接多余的`/`和对星铁攻略链接`空格`进行转义 (#29) (#481)
    * 去除武器图鉴多余的/
    
    * 星铁攻略对空格转义

--- LittlePaimon/plugins/Paimon_Wiki/wiki_api.py
+++ LittlePaimon/plugins/Paimon_Wiki/wiki_api.py
@@ -7,7 +7,7 @@ API: Dict[str, str] = {
     '角色材料': '{proxy}https://raw.githubusercontent.com/Nwflower/genshin-atlas/master/material%20for%20role/{name}.png',
     '收益曲线': '{proxy}https://raw.githubusercontent.com/CMHopeSunshine/LittlePaimonRes/main/genshin_guide/curve/{name}.jpg',
     '参考面板': '{proxy}https://raw.githubusercontent.com/CMHopeSunshine/LittlePaimonRes/main/genshin_guide/panel/{name}.jpg',
-    '武器图鉴': '{proxy}https://raw.githubusercontent.com/Nwflower/genshin-atlas/master/{name}',
+    '武器图鉴': '{proxy}https://raw.githubusercontent.com/Nwflower/genshin-atlas/master{name}',
     '圣遗物图鉴': '{proxy}https://raw.githubusercontent.com/Nwflower/genshin-atlas/master/artifact/{name}.png',
     '原魔图鉴': '{proxy}https://raw.githubusercontent.com/CMHopeSunshine/GenshinWikiMap/master/results/monster_map/{name}.jpg',
     '特产图鉴': '{proxy}https://raw.githubusercontent.com/Nwflower/genshin-atlas/master/specialty/{name}.png',
